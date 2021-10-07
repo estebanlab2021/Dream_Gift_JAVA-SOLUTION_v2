72ade72dd34b378785d4d79d08d51389a1e2db04
calvarado1234 <90731336+calvarado1234@users.noreply.github.com>
2021-10-07 01:19:57 -0300

    creacion de catpacks

---
+++ src/modelo/CatPacks.java
@@ -0,0 +1,43 @@
+/****************************************
+ ***********RRSS "DreamGift" ************
+ ***************************************/
+
+package modelo;
+
+/**
+ ******"Cristian Alvarado"*******
+ */
+
+public class CatPacks {
+    
+private int idcategoria_pack;
+private String categoria_pack;
+private int estado_pack;
+
+    public int getIdcategoria_pack() {
+        return idcategoria_pack;
+    }
+
+    public void setIdcategoria_pack(int idcategoria_pack) {
+        this.idcategoria_pack = idcategoria_pack;
+    }
+
+    public String getCategoria_pack() {
+        return categoria_pack;
+    }
+
+    public void setCategoria_pack(String categoria_pack) {
+        this.categoria_pack = categoria_pack;
+    }
+
+    public int getEstado_pack() {
+        return estado_pack;
+    }
+
+    public void setEstado_pack(int estado_pack) {
+        this.estado_pack = estado_pack;
+    }
+
+
+
+}
