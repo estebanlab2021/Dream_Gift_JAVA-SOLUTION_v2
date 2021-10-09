cc5c8ff37e305ef1bba835cc4e2fa3c15b1014ca
Esteban Arroyo <estebanlab@gmail.com>
2021-10-08 21:39:17 -0300

    Ajustes Consultas Ventas

--- src/modelo/ConsultasVentas.java
+++ src/modelo/ConsultasVentas.java
@@ -13,21 +13,22 @@ public class ConsultasVentas extends Conexion{
         PreparedStatement ps = null;
         Connection con = (Connection) getConexion();
         
-        String sql ="INSERT INTO venta (rut_cliente, id_rrss, id_pack, id_estados_venta, vta_nombre_destinatario, vta_fecha_entrega, vta_direccion_destinatario, id_comuna, vta_saludo, vta_hora_entrega_inicial, vta_hora_entrega_final, id_banco) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        String sql ="INSERT INTO venta (rut_cliente, id_rrss, id_pack, vta_total, id_estados_venta, vta_nombre_destinatario, vta_fecha_entrega, vta_direccion_destinatario, id_comuna, vta_saludo, vta_hora_entrega_inicial, vta_hora_entrega_final, id_banco) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
         try{
             ps = con.prepareStatement(sql);
             ps.setString(1, vtn.getRut_cliente());
             ps.setInt(2, vtn.getId_rrss());
             ps.setInt(3, vtn.getId_pack());
-            ps.setInt(4, vtn.getId_estados_venta());
-            ps.setString(5, vtn.getVta_nombre_destinatario());
-            ps.setString(6, vtn.getVta_fecha_entrega());
-            ps.setString(7, vtn.getVta_direccion_destinatario());
-            ps.setInt(8, vtn.getId_comuna());
-            ps.setString(9, vtn.getVta_saludo());
-            ps.setString(10, vtn.getVta_hora_entrega_inicial());
-            ps.setString(11, vtn.getVta_hora_entrega_final());
-            ps.setInt(12, vtn.getId_banco());
+            ps.setDouble(4, vtn.getVta_total());
+            ps.setInt(5, vtn.getId_estados_venta());
+            ps.setString(6, vtn.getVta_nombre_destinatario());
+            ps.setString(7, vtn.getVta_fecha_entrega());
+            ps.setString(8, vtn.getVta_direccion_destinatario());
+            ps.setInt(9, vtn.getId_comuna());
+            ps.setString(10, vtn.getVta_saludo());
+            ps.setString(11, vtn.getVta_hora_entrega_inicial());
+            ps.setString(12, vtn.getVta_hora_entrega_final());
+            ps.setInt(13, vtn.getId_banco());
 
             
             ps.execute();
