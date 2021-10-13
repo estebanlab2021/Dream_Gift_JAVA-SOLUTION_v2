b93462e2406bd6adcd5e1aec917a35343b4f76e3
calvarado1234 <90731336+calvarado1234@users.noreply.github.com>
2021-10-13 17:44:00 -0300

    modificacion de codigo.

--- src/modelo/CatPacks.java
+++ src/modelo/CatPacks.java
@@ -1,8 +1,10 @@
-/****************************************
- ***********RRSS "DreamGift" ************
- ***************************************/
-
 package modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  ******"Cristian Alvarado"*******
@@ -38,6 +40,25 @@ private int estado_pack;
         this.estado_pack = estado_pack;
     }
 
-
+//Metodo para Agregar datos al ComboBOXArticulo
+    public static ArrayList<String> MostrarCategoriasPack(){
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Conexion conn = new Conexion();
+        Connection con = conn.getConexion();
+        ArrayList<String> datos = new ArrayList<String>();
+        try{
+            String sql = "SELECT * FROM categoria_pack";
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                datos.add(rs.getString("categoria_pack"));
+            }
+            rs.close();
+        }catch(SQLException ex){
+            System.err.println(ex.toString());
+        }
+        return datos;
+    }
 
 }
