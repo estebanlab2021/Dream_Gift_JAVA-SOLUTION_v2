60b6dc46951c4a1bcbcd0565296ca721f11b40a0
Esteban Arroyo <estebanlab@gmail.com>
2021-10-13 21:08:32 -0300

    Agregar Consulta Usuario

---
+++ src/modelo/Usuarios.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Usuarios {
+    
+    private int idusuario;
+    private String usuario;
+    private String password;
+    private int user_estado;
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUser_estado() {
+        return user_estado;
+    }
+
+    public void setUser_estado(int user_estado) {
+        this.user_estado = user_estado;
+    }
+
+   
+    
+    
+    
+}
